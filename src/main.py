94403d3c18a70796c98be5dc7da5cffaf077f4b7
ajrollerson <alistair.jrollerson@hotmail.co.uk>
2026-05-08 15:32:00 +0700

    Add TextNode class and TextType enum

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode, TextType
+
+def main():
+    test = TextNode("This is some anchor text", TextType.PLAIN_TEXT)
+    print(test)
+
+
+
+
+if __name__ == "__main__":
+    main()
