3348cfcbad382de9c0991348152d30564c30ca18
ajrollerson <alistair.jrollerson@hotmail.co.uk>
2026-05-08 17:35:08 +0700

    Add LeafNode class and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -15,6 +15,21 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode(props={"href": "https://google.com", "target": "_blank"})
         self.assertEqual(node.props_to_html(), ' href="https://google.com" target="_blank"')
     
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Hello, world!")
+        self.assertEqual(node.to_html(), "<a>Hello, world!</a>")
+
+    def test_lead_to_html_none(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_lead_to_html_withprop(self):
+        node = LeafNode("a", "Hello, world!", props={"href": "https://google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://google.com">Hello, world!</a>')
 
 if __name__ == "__main__":
     unittest.main()
